9e5f52538fbb1a84c564c5617a0b500119c959e1
Dennis Chung <chungdennis6@gmail.com>
2019-04-07 21:05:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,10 @@ class Song
     self.all.find{|x| x.name == name}
   end
   
-  def Song.find_or_create_by_name
+  def Song.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end
+  
+  def Song.alphabetical
+    
 end
