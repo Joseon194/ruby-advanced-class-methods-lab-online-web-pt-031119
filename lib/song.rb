467b5eb8a62d9b3c841a724a8fa21bfc570d656f
Dennis Chung <chungdennis6@gmail.com>
2019-04-07 21:19:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,4 +50,9 @@ class Song
   
   def Song.create_from_filename(name)
     song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
 end
